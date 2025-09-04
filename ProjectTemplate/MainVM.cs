b923cbdb68f609b1d782737effa6246b3ffc9f40
DESKTOP-BR9BI07\CalvinPC <caca123tw@gmail.com>
2025-09-04 12:51:08 +0800

    wpf .netframework

---
+++ ProjectTemplate/MainVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate
+{
+    public class MainVM: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<string> _Items;
+        public ObservableCollection<string> Items
+        {
+            get => _Items;
+            set
+            {
+                if (_Items != value)
+                {
+                    _Items = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _AppName;
+
+        public string AppName
+        {
+            get { return _AppName; }
+            set
+            {
+                if (_AppName != value)   // 避免重複觸發
+                {
+                    _AppName = value;
+                    OnPropertyChanged(); // 通知綁定的 UI 更新
+                }
+            }
+        }
+
+        public MainVM()
+        {
+            AppName = "test";
+        }
+    }
+}
